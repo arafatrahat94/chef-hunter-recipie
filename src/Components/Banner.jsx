b83d3b88256fa192322f38c812d3c7f382ab6907
Arafath Hossain <arafatrahat94@gmail.com>
2023-08-27 01:43:23 -0700

    home page banenr part with text is reaady

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,32 +1,45 @@
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectFade, Autoplay } from "swiper/modules";
+import { Autoplay } from "swiper/modules";
 const Banner = () => {
+  const [bigBanner, setBigBanner] = useState([]);
+  useEffect(() => {
+    fetch(`https://chef-server-eight-beta.vercel.app/bannerBig`)
+      .then((res) => res.json())
+      .then((data) => setBigBanner(data));
+  }, []);
   return (
-    <div>
-      <>
-        <Swiper
-          slidesPerView={1}
-          spaceBetween={30}
-          loop={true}
-          pagination={{
-            clickable: true,
-          }}
-          autoplay={{
-            delay: 3500,
-            disableOnInteraction: false,
-          }}
-          effect={"fade"}
-          modules={[EffectFade, Autoplay]}
-          className="mySwiper"
-        >
-          <SwiperSlide>Slide 1</SwiperSlide>
-        </Swiper>
-      </>
+    <div className="">
+      <div className="">
+        <>
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={30}
+            loop={true}
+            pagination={{
+              clickable: true,
+            }}
+            autoplay={{
+              delay: 4500,
+              disableOnInteraction: false,
+            }}
+            modules={[Autoplay]}
+            className="mySwiper p-0 h-[500px]  bg-black"
+          >
+            {bigBanner.map((img) => (
+              <>
+                <SwiperSlide>
+                  <img src={img} className="opacity-40  w-full" alt="" />
+                </SwiperSlide>
+              </>
+            ))}
+          </Swiper>
+        </>
+      </div>
     </div>
   );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,18 @@ import Banner from "../../Components/Banner";
 const Home = () => {
   return (
     <div>
+      <div className="absolute backdrop-blur p-3 pb-4 border-base-300 border border-e-0 rounded-s-3xl right-0 z-10 top-[120px] w-[60%]">
+        <h1 className="font-DancingS ps-2 pt-2 text-5xl text-pink-500">
+          Welcome
+        </h1>
+        <h2 className="font-Nunito pb-4 tracking-wide text-[40px] ms-14 text-white font-semibold">
+          Get American Tasty Foods From{" "}
+          <span className="text-6xl tracking-normal text-pink-500">
+            Foody World
+          </span>
+          .
+        </h2>
+      </div>
       <Banner></Banner>
     </div>
   );
