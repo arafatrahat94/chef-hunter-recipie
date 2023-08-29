05a86738a12786c6a3a88aa8fcf548543d936e80
Arafath Hossain <arafatrahat94@gmail.com>
2023-08-28 22:17:52 -0700

    darkmode and light mode button reload set to darkmode icon in both mode fixed now

--- src/Components/DarkmodeButton.jsx
+++ src/Components/DarkmodeButton.jsx
@@ -3,14 +3,14 @@ import { FaRegMoon } from "react-icons/fa6";
 import { BsSunFill } from "react-icons/bs";
 
 const DarkmodeButton = () => {
-  const [isDarkMode, setIsDarkMode] = useState(() => false);
+  const [isDarkMode, setIsDarkMode] = useState(() => true);
   const [theme, setTheme] = useState(
-    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "black"
   );
-
+  console.log(theme);
   // update state on toggle
   const handleToggle = (e) => {
-    if (e.target.checked) {
+    if (theme === "light") {
       setTheme("black");
     } else {
       setTheme("light");
@@ -30,9 +30,17 @@ const DarkmodeButton = () => {
         <label className="swap swap-rotate w-12 h-12">
           <input type="checkbox" onChange={handleToggle} />
           {/* light theme sun image */}
-          <BsSunFill className="w-8 h-8 swap-on" />
+          {theme === "black" ? (
+            <BsSunFill
+              className={`w-8 h-8 ${theme === "black" ? "block" : "hidden"}`}
+            />
+          ) : (
+            <FaRegMoon
+              className={`w-8 h-8 ${theme === "black" ? "hidden" : "block"}`}
+            />
+          )}
+
           {/* dark theme moon image */}
-          <FaRegMoon className="w-8 h-8 swap-off" />
         </label>
       </button>
     </div>
